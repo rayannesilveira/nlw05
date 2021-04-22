4767c02d70a6aeb6a2410c9cf26274796e11099d
Rayanne Silveira <S1lv31r4123>
2021-04-21 23:42:32 -0300

    feat(): aula 02

---
+++ lib/home/widgets/quiz_card/quiz_card_widget.dart
@@ -0,0 +1,49 @@
+import 'package:DevQuiz/core/core.dart';
+import 'package:DevQuiz/shared/widgets/progress_indicator/progress_indicator_widget.dart';
+import 'package:flutter/material.dart';
+
+class QuizCardWidget extends StatelessWidget {
+  const QuizCardWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+          border: Border.fromBorderSide(BorderSide(color: AppColors.border)),
+          color: AppColors.white,
+          borderRadius: BorderRadius.circular(10)),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+              width: 40, height: 40, child: Image.asset(AppImages.blocks)),
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            "Gerenciamento de Estado",
+            style: AppTextStyles.heading15,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            children: [
+              Expanded(
+                  flex: 2,
+                  child: Text(
+                    "3 de 10",
+                    style: AppTextStyles.body11,
+                  )),
+              Expanded(
+                flex: 4,
+                child: ProgressIndicatorWidget(value: 0.3)
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
